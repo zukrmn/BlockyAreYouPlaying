3746f53d09115ce3ee9c8f2170d59fb0b086139e
Andrade Core <phiandrade@proton.me>
2025-11-15 21:18:10 +0000

    Renomeado tudo relacionado ao AuthPlugin

--- src/main/java/com/blockycraft/blockyareyouplaying/PlayerCheckTask.java
+++ src/main/java/com/blockycraft/blockyareyouplaying/PlayerCheckTask.java
@@ -1,6 +1,6 @@
 package com.blockycraft.blockyareyouplaying;
 
-import com.blocky.auth.AuthPlugin;
+import com.blockycraft.blockyauth.BlockyAuth;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -8,24 +8,24 @@ public class PlayerCheckTask implements Runnable {
 
     private final BlockyAreYouPlaying plugin;
     private final DatabaseManager dbManager;
-    private final AuthPlugin authPlugin;
+    private final BlockyAuth blockyAuth;
 
     public PlayerCheckTask(BlockyAreYouPlaying plugin, DatabaseManager dbManager) {
         this.plugin = plugin;
         this.dbManager = dbManager;
         
-        Plugin authPluginInstance = plugin.getServer().getPluginManager().getPlugin("AuthPlugin");
-        if (authPluginInstance instanceof AuthPlugin) {
-            this.authPlugin = (AuthPlugin) authPluginInstance;
+        Plugin blockyAuthInstance = plugin.getServer().getPluginManager().getPlugin("BlockyAuth");
+        if (blockyAuthInstance instanceof BlockyAuth) {
+            this.blockyAuth = (BlockyAuth) blockyAuthInstance;
         } else {
-            throw new RuntimeException("AuthPlugin not found!");
+            throw new RuntimeException("BlockyAuth not found!");
         }
     }
 
     @Override
     public void run() {
         for (Player player : plugin.getServer().getOnlinePlayers()) {
-            if (authPlugin.isAuthenticated(player)) {
+            if (blockyAuth.isAuthenticated(player)) {
                 dbManager.logPlayer(player);
             }
         }
